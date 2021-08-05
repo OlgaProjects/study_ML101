dd2dee8ed31d04ebe825b988670cf6a2735dd353
Olga <fkoo@bk.ru>
2021-08-05 21:18:05 +0700

    edit file with PEP8 standart

--- Module03/lesson03/practice 2/vehicles_helper.py
+++ Module03/lesson03/practice 2/vehicles_helper.py
@@ -4,17 +4,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
 from scipy.stats import norm
+
 get_ipython().run_line_magic('matplotlib', 'inline')
 sns.set()
 
 import sklearn.metrics as metrics
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report 
-from sklearn.model_selection import train_test_split 
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 from sklearn.svm import SVC
 
 import warnings
+
 warnings.filterwarnings('ignore')
 
 
@@ -27,9 +29,8 @@ def target_attributes_counts(dataset):
     Parameters:
         dataset.
     """
-    print("'Class' Value Counts: "+" \n", dataset['Class'].value_counts())
-    print("\n Visualisation plot: "+" \n", dataset['Class'].value_counts().plot(x = dataset['Class'], kind='bar'))
-   
+    print("'Class' Value Counts: " + " \n", dataset['Class'].value_counts())
+    print("\n Visualisation plot: " + " \n", dataset['Class'].value_counts().plot(x=dataset['Class'], kind='bar'))
 
 
 def all_attrubutes_vizual(dataset, one, two, three):
@@ -44,25 +45,25 @@ def all_attrubutes_vizual(dataset, one, two, three):
         two: selected attributes for part two 
         three: selected attributes for part three
     """
-    print("Part one:"+"\n")
-    df1 = dataset[one] 
-    sns.pairplot(df1, kind="scatter",  hue="Class", plot_kws=dict(s=80, edgecolor="white", linewidth=2.5))
+    print("Part one:" + "\n")
+    df1 = dataset[one]
+    sns.pairplot(df1, kind="scatter", hue="Class", plot_kws=dict(s=80, edgecolor="white", linewidth=2.5))
     plt.show()
     print("\n")
-    print("Part two:"+"\n")
-    df2 = dataset[two] 
-    sns.pairplot(df2, kind="scatter",  hue="Class", plot_kws=dict(s=80, edgecolor="white", linewidth=2.5))
+    print("Part two:" + "\n")
+    df2 = dataset[two]
+    sns.pairplot(df2, kind="scatter", hue="Class", plot_kws=dict(s=80, edgecolor="white", linewidth=2.5))
     plt.show()
     print("\n")
-    print("Part three:"+"\n")
-    df3 = dataset[three] 
-    sns.pairplot(df3, kind="scatter",  hue="Class", plot_kws=dict(s=80, edgecolor="white", linewidth=2.5))
+    print("Part three:" + "\n")
+    df3 = dataset[three]
+    sns.pairplot(df3, kind="scatter", hue="Class", plot_kws=dict(s=80, edgecolor="white", linewidth=2.5))
     plt.show()
-    
-   
-def corr_plot_list(dataset): 
+
+
+def corr_plot_list(dataset):
     """ This module presents correlation plot and list of each attribute"""
-    
+
     print("'Correlation list of each attribute: ")
     corr = dataset.corr()
     corr_abs = corr.abs()
@@ -70,7 +71,5 @@ def corr_plot_list(dataset):
     num_corr = corr_abs.nlargest(num_cols, 'Class')['Class']
     print(num_corr)
     print("\n")
-    print("'Correlation plot of each attribute: "+"\n", dataset.corr()['Class'].sort_values().plot(kind='bar', figsize=(18, 6)))
-
-
-
+    print("'Correlation plot of each attribute: " + "\n",
+          dataset.corr()['Class'].sort_values().plot(kind='bar', figsize=(18, 6)))
